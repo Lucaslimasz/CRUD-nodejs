b70cfbea86db3794f5af0da054b466c6c760bac1
9ideia <lucasl@9ideia.com.br>
2020-06-25 08:55:11 -0300

    paginate e cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
+
 
 // inicinado o app
 const app = express();
 // permite que envie dados em json
 app.use(express.json());
+app.use(cors())
 
 // iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', 
